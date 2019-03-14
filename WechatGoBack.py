3f89f44d1f4de587644a26a51011b0c012d89bfa
WatsonJay <446725026@qq.com>
2019-03-14 21:46:47 +0800

    Update WechatGoBack.py

--- WechatGoBack.py
+++ WechatGoBack.py
@@ -3,7 +3,7 @@ import re
 import os
 import time
 import itchat
-from itchat.content import TEXT, PICTURE, FRIENDS, CARD, MAP, SHARING, RECORDING, ATTACHMENT, VIDEO, NOTE
+from itchat.content import TEXT, FRIENDS, ATTACHMENT, VIDEO, RECORDING, PICTURE, CARD, MAP, SHARING, NOTE
 
 MSGINFO = {}
 FACEPACKAGE = None
@@ -42,7 +42,28 @@ class WechatGoBack():
                     break
             if not group_name:
                 group_name = u'未命名群聊'
-                
+            group_name = group_name + u'(%s)人' % str(group_member)
+            msg_from = group_name + '-->' + msg_from
+        else:
+            try:
+                msg_from = itchat.search_friends(userName=msg['FromUserName'])['RemarkName']
+                if not msg_from:
+                    msg_from = itchat.search_friends(userName=msg['FromUserName'])['NickName']
+            except:
+                msg_from = u'微信官方消息'
+        msg_send_time = msg['CreateTime']
+        msg_id = msg['MsgId']
+        msg_content = None
+        msg_link = None
+        if msg['Type'] == 'Text' or msg['Type'] == 'Friends':
+               msg_content = msg['Text']
+               print('[TEXT/FRIENDS]:%S' % msg_content)
+        elif msg['Type'] == 'ATTACHMENT' or msg['Type'] == 'VIDEO' or msg['Type'] == 'RECORDING' or msg['Type'] == 'PICTURE':
+                msg_content = msg['FileName']
+                msg['Text'](str(msg_content))
+                print('[Attachment/Video/Picture/Recording]: %s' % msg_content)
+                p=1
+
 if __name__ == '__main__':
     test = WechatGoBack()
     test.run()
